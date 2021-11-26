986228c5cf1f9004cc02e41ce4a63d1df6afda24
CarloLim <CarloLim01>
2021-11-26 17:38:12 +0800

    Program 2 (Processing the total score of the user)

--- Program2.py
+++ Program2.py
@@ -34,3 +34,21 @@ def start():
         else:
             user_score = user_score    
             print(f"Hi {user_name}, you're wrong. The correct answer is {question}.") 
+
+    print("\nPlease wait! We are processing your score.")
+    print(input("\nPress 'enter' to proceed"))
+    
+    if user_score == 10:
+        print(f"{user_name}, you got the total score of {user_score}/10.")
+        print(f"\nExcellent! You got the perfect score.")
+    elif user_score >= 5 and user_score == 9:
+        print(f"{user_name}, you got the total score of {user_score}/10.")
+        print(f"\nVery Good! Keep it up.")
+    elif user_score >= 1 and user_score == 4:
+        print(f"{user_name}, you got the total score of {user_score}/10.")
+        print(f"\nGood! Needs improvement.")
+    else:
+        print(f"{user_name}, you got the total score of {user_score}/10.")
+        print(f"\nBetter luck next time.")
+
+start()
