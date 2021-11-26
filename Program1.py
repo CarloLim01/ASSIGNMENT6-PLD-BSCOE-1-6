bbec0fb75271910e9d2b0a8379a614b6f47e0aa0
CarloLim <CarloLim01>
2021-11-26 14:44:24 +0800

    Program 1 (Adding outcomes if the 1st number is the highest)

--- Program1.py
+++ Program1.py
@@ -5,3 +5,17 @@ first = float(input("\nEnter the first number: "))
 second = float(input("Enter the second number: "))
 third = float(input("Enter the third number: "))
 fourth = float(input("Enter the fourth number: "))
+
+def number_sorter(first, second, third, fourth):
+    if first >= second and second >= third and third >= fourth:
+        print(f"\nSort from Highest to Lowest: {first}, {second}, {third}, {fourth}")
+    elif first >= second and second >= fourth and fourth >= third:
+        print(f"\nSort from Highest to Lowest: {first}, {second}, {fourth}, {third}")
+    elif first >= third and third >= second and second >= fourth:
+        print(f"\nSort from Highest to Lowest: {first}, {third}, {second}, {fourth}")
+    elif first >= third and third >= fourth and fourth >= second:
+        print(f"\nSort from Highest to Lowest: {first}, {third}, {fourth}, {second}")
+    elif first >= fourth and fourth >= second and second >= third:
+        print(f"\nSort from Highest to Lowest: {first}, {fourth}, {second}, {third}")
+    elif first >= fourth and fourth >= third and third >= second:
+        print(f"\nSort from Highest to Lowest: {first}, {fourth}, {third}, {second}")
