44e675f30bfaf9ecac3cc26619d973a618081fa1
CarloLim <CarloLim01>
2021-11-26 14:38:35 +0800

    Program 1 (Asking the 4 numbers from the user)

---
+++ Program1.py
@@ -0,0 +1,7 @@
+# Create a program that ask 4 numbers. 
+# Print the 4 numbers from highest to lowest using only if-else statement.
+
+first = float(input("\nEnter the first number: "))
+second = float(input("Enter the second number: "))
+third = float(input("Enter the third number: "))
+fourth = float(input("Enter the fourth number: "))
