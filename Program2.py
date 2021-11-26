0be4272fe394ef7b4ba9c8940d3b4aecf6de2b09
CarloLim <CarloLim01>
2021-11-26 17:06:00 +0800

    Program 2 (Adding inputs and Asking user name)

---
+++ Program2.py
@@ -0,0 +1,10 @@
+# Create a program that automatically generate two random numbers to add (0 to 99)
+# Let the user question and evaluate if the user has the correct question
+# The program will ask the user 10 addition operation
+# Display the result summary of the 10 operations (ex 9/10)
+
+def start():
+    user_name = input("\nEnter your name: ")
+    print(f"\nHI {user_name}, WELCOME TO ADDITION QUIZ!")
+    print("\nThere would be 10 random addition question that you need to answer. ")
+    print(input("\nPress 'enter' to proceed"))
